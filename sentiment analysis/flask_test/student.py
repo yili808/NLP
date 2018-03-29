804ebba40796421cb676113b8b25e0d4337122e2
Yi Li <yl808@georgetown.edu>
2018-03-29 14:14:54 -0400

    Add files via upload

---
+++ sentiment analysis/flask_test/student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 27 11:59:03 2018
+
+@author: yili
+"""
+
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def student():
+   return render_template('student.html')
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      return render_template("result.html",result = result)
+
+if __name__ == '__main__':
+   app.run(debug = True)
